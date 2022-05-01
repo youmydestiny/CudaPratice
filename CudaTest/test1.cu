8f3acfdc5c90256a038cfdbba04294e2589cfb5b
Dell <Dell@DELL>
2022-05-01 16:51:40 +0800

    添加项目文件。

---
+++ CudaTest/test1.cu
@@ -0,0 +1,59 @@
+#include "cuda_runtime.h" 
+#include "device_launch_parameters.h"
+#include "cublas_v2.h"  
+
+#include <time.h>  
+#include <iostream>  
+
+// 两个向量加法kernel，grid和block均为一维
+__global__ void add(float* x, float* y, float* z, int n)
+{
+    // 获取全局索引
+    int index = threadIdx.x + blockIdx.x * blockDim.x;
+    // 步长
+    int stride = blockDim.x * gridDim.x;
+    for (int i = index; i < n; i += stride)
+    {
+        z[i] = x[i] + y[i];
+    }
+}
+
+int main()
+{
+    int N = 1 << 20;
+    int nBytes = N * sizeof(float);
+
+    // 申请托管内存
+    float* x, * y, * z;
+    cudaMallocManaged((void**)&x, nBytes);
+    cudaMallocManaged((void**)&y, nBytes);
+    cudaMallocManaged((void**)&z, nBytes);
+
+    // 初始化数据
+    for (int i = 0; i < N; ++i)
+    {
+        x[i] = 10.0;
+        y[i] = 20.0;
+    }
+
+    // 定义kernel的执行配置
+    dim3 blockSize(256);
+    dim3 gridSize((N + blockSize.x - 1) / blockSize.x);
+    // 执行kernel
+    add << < gridSize, blockSize >>> (x, y, z, N);
+
+    // 同步device 保证结果能正确访问
+    cudaDeviceSynchronize();
+    // 检查执行结果
+    float maxError = 0.0;
+    for (int i = 0; i < N; i++)
+        maxError = fmax(maxError, fabs(z[i] - 31.0));
+    std::cout << "最大误差: " << maxError << std::endl;
+
+    // 释放内存
+    cudaFree(x);
+    cudaFree(y);
+    cudaFree(z);
+
+    return 0;
+}
